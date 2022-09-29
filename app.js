2f5d5b642574085c9f2f9481ec612c4a9928292f
Ky Anh <anhb1910342@student.ctu.edu.vn>
2022-09-29 15:59:18 +0700

    cai dat handelr truy xuat CSDL

--- app.js
+++ app.js
@@ -9,9 +9,11 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactsRouter);
 app.use((req, res, next) => {
+    //ham error se chuyen vao neu sai duong dan
     return next(new ApiError(404, "Resource not found"));
 });
 app.use((err, req, res, next) => {
+    //hm khi may chu khong the cung cap 
     return res.status(err.statusCode || 500).json({
         message: err.message || "Internal Server Error",
     }); 
